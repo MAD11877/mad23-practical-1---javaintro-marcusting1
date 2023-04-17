1dff9e1c96f4f979ab3b7ff5f4b5223b8cfa08b0
marcusting1 <130948600+marcusting1@users.noreply.github.com>
2023-04-17 10:54:29 +0800

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -1,7 +1,7 @@
-public class Question1
-{
-  public static void main(String[] args)
-  {
-    System.out.println("Hello World");
-  }
+public class Question1 
+{  
+  public static void main(String args[])
+  { 
+    System.out.println("Hello, world!"); 
+  } 
 }
