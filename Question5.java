912c2c43e4460cd64d9f2212d38c347700a7c6bc
marcusting1 <130948600+marcusting1@users.noreply.github.com>
2023-04-17 21:55:41 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,32 +1,51 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.lang.Math;
 
-public class Question5
-{
-  public static void main(String[] args)
-  {
-    /**
-     * Prompt the user for number. This input indicates the number of integers the user will be entering next. 
-     * Print out the mode (highest occurrence) from the set of integers. 
-     *    e.g.
-     *     > 5
-     *     > 2
-     *     > 4
-     *     > 1
-     *     > 3
-     *     > 4
-     *     4
-     * 
-     *    e.g.
-     *     > 4
-     *     > 2
-     *     > 2
-     *     > 3
-     *     > 3
-     *     2
-     * Hint: Use a loop to get input. Use another 2 loops to find the mode
-     */
-     
-    Scanner in = new Scanner(System.in);
+class Main {
+  public static void main(String args[]) { 
+    double highest = Math.pow(-999999,999999);
     
-  }
+    ArrayList<Integer> intList = new ArrayList<Integer>();
+    ArrayList<Integer> freqList = new ArrayList<Integer>();
+
+    Scanner in = new Scanner(System.in);
+    int num = in.nextInt();
+
+    for (int i = num; i >= 1; i--){
+      Scanner in1 = new Scanner(System.in);
+      int integers = in1.nextInt();
+      intList.add(integers);
+    }
+
+    //System.out.println(intList.size());
+
+    for (int j = 0 ; j< intList.size(); j++){
+      num = intList.get(j);
+      int count = 0;
+      for (int k = 0; k< intList.size(); k++){
+        //count = 0;
+        int val = intList.get(k);
+        if (val==num){
+          count++;
+        }
+      }
+      freqList.add(count);
+    }
+    for (int l = 0 ; l < freqList.size(); l++){
+      if (freqList.get(l) > highest){
+        highest = freqList.get(l);
+      }
+    }
+
+    int mode = 0;
+    int newhighest = (int)highest;
+    
+    for (int m = 0 ; m < freqList.size(); m++){
+      if (newhighest == freqList.get(m)){
+        mode = intList.get(m);
+      }
+    }
+  System.out.println(mode);
+  } 
 }
